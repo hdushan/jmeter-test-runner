1098fe5681b9c444c11b8c3924f370de3ffc2ac7
Hans Dushanthakumar <hans@thoughtworks.com>
2015-01-10 13:17:28 +0200

    Pass in options from command line

--- lib/jmeter-test-runner.rb
+++ lib/jmeter-test-runner.rb
@@ -7,23 +7,28 @@ module JmeterTestRunner
 
     attr_reader :jmeter_path
     
-    def initialize(jmeter_test_plan, jmeter_test_result, jmeter_test_result_format, jmeter_html_test_result, jmeter_version='2.12')
+    def initialize(jmeter_test_plan, jmeter_test_result, jmeter_test_result_format, jmeter_html_test_result, options='', jmeter_version='2.12')
       @jmeter_test_plan = jmeter_test_plan
       @jmeter_test_result = jmeter_test_result
       @jmeter_test_result_format = jmeter_test_result_format
       @jmeter_version = jmeter_version
-      @jmeter_binary_url = "http://ftp.itu.edu.tr/Mirror/Apache//jmeter/binaries/apache-jmeter-#{@jmeter_version}.tgz"
+      @jmeter_installer = "apache-jmeter-#{@jmeter_version}.tgz"
+      @jmeter_binary_url = "http://ftp.itu.edu.tr/Mirror/Apache//jmeter/binaries/#{@jmeter_installer}"
       @jmeter_standard_plugin = "JMeterPlugins-Standard-1.2.0.zip"
-      @jmeter_standard_plugin_url = "http://jmeter-plugins.org/downloads/file/" + @jmeter_standard_plugin
+      @jmeter_standard_plugin_url = "http://jmeter-plugins.org/downloads/file/#{@jmeter_standard_plugin}"
       @jmeter_extras_plugin = "JMeterPlugins-Extras-1.2.0.zip"
-      @jmeter_extras_plugin_url = "http://jmeter-plugins.org/downloads/file/" + @jmeter_extras_plugin
+      @jmeter_extras_plugin_url = "http://jmeter-plugins.org/downloads/file/#{@jmeter_extras_plugin}"
       @jmeter_workspace = ENV['HOME']
       @jmeter_install_folder = "apache-jmeter-#{jmeter_version}"
       @jmeter_executable_file = "jmeter"
       @jmeter_standard_plugin_file = "#{@jmeter_workspace}/#{@jmeter_install_folder}/lib/ext/JMeterPlugins-Standard.jar"
       @jmeter_extras_plugin_file = "#{@jmeter_workspace}/#{@jmeter_install_folder}/lib/ext/JMeterPlugins-Extras.jar"
       @jmeter_path = "#{@jmeter_workspace}/#{@jmeter_install_folder}/bin"
-      @jmeter_command = "#{@jmeter_path}/#{@jmeter_executable_file}"
+      if options != ''
+        @jmeter_command = "#{@jmeter_path}/#{@jmeter_executable_file} -J#{options}"
+      else
+        @jmeter_command = "#{@jmeter_path}/#{@jmeter_executable_file}"
+      end
       @jmeter_xslt_template_file = "#{@jmeter_workspace}/#{@jmeter_install_folder}/extras/jmeter-results-detail-report_21.xsl"
       @jmeter_html_output_file = jmeter_html_test_result
     end
@@ -54,19 +59,21 @@ module JmeterTestRunner
     
     def is_jmeter_installed?
       puts "\nChecking for presence of jmeter executable file #{@jmeter_command}\n"
-      if is_os_supported?
-        return File.file? "#{@jmeter_command}"
-      else
-        puts "\nJmeter not found (in folder #{@jmeter_workspace})."
-        puts "This gem cannot install jmeter automatically on this OS (yet..coming soon!)."
-        puts "Please install jmeter (version #{@jmeter_version}) manually into folder #{@jmeter_workspace}\n"
-        puts "\nCannot proceed. Hence exiting...\n"
-        raise
+      jmeter_installed = File.file? "#{@jmeter_command}"
+      if !is_os_supported?
+        if !jmeter_installed
+          puts "\nJmeter not found (in folder #{@jmeter_workspace})."
+          puts "This gem cannot install jmeter automatically on this OS (yet..coming soon!)."
+          puts "Please install jmeter (version #{@jmeter_version}) manually into folder #{@jmeter_workspace}\n"
+          raise "\nCannot proceed. Hence exiting...\n"
+        end
       end
+      return jmeter_installed
     end
     
     def is_jmeter_standard_plugin_installed?
       puts "\nChecking for presence of jmeter standard plugin #{@jmeter_standard_plugin_file}\n"
+      plugin_installed = File.file? "#{@jmeter_standard_plugin_file}"
       if is_os_supported?
         return File.file? "#{@jmeter_standard_plugin_file}"
       else
